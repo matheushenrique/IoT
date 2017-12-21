65c6ef2e370050ff632d0af467b79d79f1a53cc5
matheushenrique <matheus.henriquepr@gmail.com>
2017-12-21 00:27:12 -0300

    funcionando para apresentacao

--- Watch Extension/InterfaceController.swift
+++ Watch Extension/InterfaceController.swift
@@ -11,11 +11,10 @@ import Foundation
 import CoreLocation
 import MapKit
 import UIKit
-
 class InterfaceController: WKInterfaceController,CLLocationManagerDelegate {
     // TODO: realizar captacao da localizacao em background de x em x minutos
     @IBOutlet var mapa: WKInterfaceMap!
-    private var locman = CLLocationManager()
+    var locman : CLLocationManager!
     private var startTime: Date? //An instance variable, will be used as a previous location time.
     var origem : CLLocationCoordinate2D!
     var destino : CLLocationCoordinate2D!
@@ -30,23 +29,30 @@ class InterfaceController: WKInterfaceController,CLLocationManagerDelegate {
         client.sendToken = true
         client.autoBlock1SZX = 2
         self.coapClient = client
+        self.locman = CLLocationManager()
         // detailValue represents the type of message: GET, POST ,...
-        locman.delegate = self
-        locman.desiredAccuracy = kCLLocationAccuracyBest // You can change the locaiton accuary here.
-        locman.startUpdatingLocation()
-        
+        self.locman.delegate = self
+        self.locman.desiredAccuracy = kCLLocationAccuracyBest // You can change the locaiton accuary here.
+        self.locman.startUpdatingLocation()
+//        print(locman)
+//        locman.requestWhenInUseAuthorization()
+//        self.locman.requestLocation()
+//        print(self.locman.location)
+//        print(selflocman.requestLocation())
         print(locman.location?.coordinate.latitude ?? "sem latitudee")
         let lat = locman.location?.coordinate.latitude ?? 0.0
         let lon = locman.location?.coordinate.longitude ?? 0.0
-        let localizacao = "\(lat),\(lon)"
+        let idDispositivo = WKInterfaceDevice.current().hashValue
+//        let localizacao = "\(lat),\(lon)"
+        let localizacao = "\(idDispositivo),\(lat),\(lon)"
         print(localizacao)
-        let m = SCMessage(code: SCCodeValue(classValue: 0, detailValue: 02)!, type: .confirmable, payload: localizacao.data(using: String.Encoding.utf8))
+        let m = SCMessage(code: SCCodeValue(classValue: 0, detailValue: 01)!, type: .confirmable, payload: localizacao.data(using: String.Encoding.utf8))
         
 //        //        if let stringData = uriPathTextField.text?.data(using: String.Encoding.utf8) {
 //        // FUNCIONANDO PARA O NODEJS
 //        m.addOption(SCOption.uriPath.rawValue, data: localizacao.data(using: String.Encoding.utf8)!)
 //        //        }
-        m.addOption(SCOption.uriPath.rawValue, data: "storage".data(using: String.Encoding.utf8)!)
+        m.addOption(SCOption.uriPath.rawValue, data: "basic".data(using: String.Encoding.utf8)!)
 
         
         if  let port = UInt16("5683") {
@@ -81,11 +87,18 @@ class InterfaceController: WKInterfaceController,CLLocationManagerDelegate {
         // quando terminar de executar o servico, pode pedir um novo servico
         let lat = locman.location?.coordinate.latitude ?? 0.0
         let lon = locman.location?.coordinate.longitude ?? 0.0
-        let localizacao = "\(lat),\(lon)"
+        let idDispositivo = WKInterfaceDevice.current().hashValue
+//        let localizacao: [String: Any] = [
+//            "idDispositivo": idDispositivo,
+//            "latitude" : lat,
+//            "longitude" : lat
+//        ]
+//        var localizacao = (idDispositivo: idDispositivo, latitude: lat)
+        let localizacao = "\(idDispositivo),\(lat),\(lon)"
 //        print(localizacao)
         origem = CLLocationCoordinate2DMake(lat, lon)
         let m = SCMessage(code: SCCodeValue(classValue: 0, detailValue: 01)!, type: .confirmable, payload: localizacao.data(using: String.Encoding.utf8))
-        m.addOption(SCOption.uriPath.rawValue, data: "basic".data(using: String.Encoding.utf8)!)
+        m.addOption(SCOption.uriPath.rawValue, data: "storage".data(using: String.Encoding.utf8)!)
         if  let port = UInt16("5683") {
             let hostString = "localhost"
             coapClient.sendCoAPMessage(m, hostName:hostString,  port: port)
@@ -101,7 +114,7 @@ class InterfaceController: WKInterfaceController,CLLocationManagerDelegate {
 //        mapa.addAnnotation(artwork)
         
     }
-    func UnoDirections(){
+    func UnoDirections(destino: CLLocationCoordinate2D){
         var coordinates = [CLLocationCoordinate2D]()
         coordinates += [origem]
         coordinates += [destino]
@@ -109,8 +122,13 @@ class InterfaceController: WKInterfaceController,CLLocationManagerDelegate {
 //        coordinates += [CLLocationCoordinate2D(latitude: 41.8924847, longitude: -87.6280187)]
 //        coordinates += [restaurants[10].coordinate] //Uno's
 //        Mkpolyli
-//        let path = MK (coordinates: &coordinates, count: coordinates.count)
+//        let path = MK (coordinates: &
+//        coordinates, count: coordinates.count)
 //        mapa.addOverlay(path)
+        let coordinate = CLLocationCoordinate2DMake(destino.latitude,destino.longitude)
+        let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: coordinate, addressDictionary:nil))
+        mapItem.name = "Target location"
+        mapItem.openInMaps(launchOptions: [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving])
     }
     
     override func willActivate() {
@@ -122,26 +140,36 @@ class InterfaceController: WKInterfaceController,CLLocationManagerDelegate {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         print("MUDOU")
         guard let loc = locations.last else { return }
-        
+
         let time = loc.timestamp
-        
+
         guard let startTime = startTime else {
             self.startTime = time // Saving time of first location, so we could use it to compare later with second location time.
             return //Returning from this function, as at this moment we don't have second location.
         }
-        
+
         let elapsed = time.timeIntervalSince(startTime) // Calculating time interval between first and second (previously saved) locations timestamps.
-        
+
         if elapsed > 30 { //If time interval is more than 30 seconds
             print("Upload updated location to server")
             print(loc)
             //updateUser(location: loc) //user function which uploads user location or coordinate to server.
-            
+
             self.startTime = time //Changing our timestamp of previous location to timestamp of location we already uploaded.
-            
+
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locman.requestLocation()
         }
     }
 
@@ -154,9 +182,13 @@ extension InterfaceController: SCClientDelegate {
             if let string = NSString(data: pay as Data, encoding:String.Encoding.utf8.rawValue) {
                 payloadstring = String(string)
                 let coordenada = payloadstring.components(separatedBy: ",")
-                destino =  CLLocationCoordinate2D(latitude: Double(coordenada[0])!, longitude: Double(coordenada[1])!)
-                self.mapa.addAnnotation(destino, with: .green)
-                UnoDirections()
+                if let lat = Double(coordenada[0]), let lon = Double(coordenada[1]){
+                    destino =  CLLocationCoordinate2D(latitude: lat, longitude: lon )
+                    self.mapa.addAnnotation(destino, with: .green)
+                    UnoDirections(destino: destino)
+//                    ope
+                    
+                }
             }
         }
         let firstPartString = "Message received from \(message.hostName ?? "") with type: \(message.type.shortString())\nwith code: \(message.code.toString()) \nwith id: \(message.messageId ?? 0)\nPayload: \(payloadstring)\n"
